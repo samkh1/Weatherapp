4d569cc8d39fd439daf9dfae3ed43100d906eda2
khalifa samira <khalifasamira@MacBook-Pro-de-Samira.local>
2024-08-23 14:58:14 +0200

    add new screens

---
+++ app/src/main/java/com/sri/weatherapp/data/model/CurrentWeatherDto.kt
@@ -0,0 +1,10 @@
+package com.sri.weatherapp.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class CurrentWeather(
+    @SerializedName("time") val time: String,
+    @SerializedName("interval") val interval: String,
+    @SerializedName("temperature_2m") val tempurature: String,
+    @SerializedName("wind_speed_10m") val wind: String,
+)
